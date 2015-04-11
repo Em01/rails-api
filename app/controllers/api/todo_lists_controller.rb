6a5e10ff1ae153f273d53a36167a2c4a29d6aa38
Em01 <emma_williams1@hotmail.co.uk>
2015-04-11 14:24:03 +0100

    updated http status codes

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -12,25 +12,36 @@ class Api::TodoListsController < ApplicationController
 	def create
 		list = TodoList.new(list_params)
 		if list.save
-			render json: {
-				status: 200,
+			render status: 200, json: {
 				message: "Successfully created To-do List",
 				todo_list: list
 			}.to_json
 		else
-			render json: {
-				status: 500,
+			render status: 422, json: {
 				errors: list.errors
 			}.to_json
-			head 500
 		end
 	end
+
+	def update
+		list = TodoList.find(params[:id])
+		if list.update(list_params)
+			render status: 200, json: {
+				message: "Successfully updated",
+				todo_list: list
+			}.to_json
+		else
+			render status: 422, json: {
+				message: "The list could not be updated",
+				todo_list: list 
+			}.to_json
+	end
+end
 
 	def destroy
 		list = TodoList.find(params[:id])
 		list.destroy
-		render json: {
-			status: 200,
+			render status: 200, json: {
 			message: "Successfully deleted To-do List."
 		}.to_json
 	end
