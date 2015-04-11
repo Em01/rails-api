94b9d35804c57e713ed335a176ff72472c3f1c48
Em01 <emma_williams1@hotmail.co.uk>
2015-04-11 15:02:23 +0100

    further work on to do items controller

--- app/controllers/api/todo_items_controller.rb
+++ app/controllers/api/todo_items_controller.rb
@@ -16,6 +16,33 @@ class Api::TodoListsController < ApplicationController
 		end
 	end
 
+	def update
+		item = @list.todo_items.find(params[:id])
+		if item.update(item_params)
+			render status: 200, json: {
+				message: "Successfully updated To-do Item",
+				todo_list: @list,
+				todo_item: item
+				}.to_json
+			else
+				render status: 422, json: {
+					message: "To-do Item update failed",
+					errors: item.errors
+				}.to_json
+			}
+	end
+end
+
+	def destroy
+		item = @list.todo_items.find(params[:id])
+		item.destroy
+			render status: 200, json: {
+			message: "Successfully deleted To-do item."
+			todo_list: @list,
+			todo_item: item
+		}.to_json
+	end
+
 	private
 	def item_params
 		params.require("todo_item").permit("content")
